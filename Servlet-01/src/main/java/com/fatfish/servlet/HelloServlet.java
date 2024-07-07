924f805d41150df751557f1b88036a2cb53a86b7
Jiale <fatfishccc@gmail.com>
2024-07-07 19:33:03 +0800

    first commit

---
+++ Servlet-01/src/main/java/com/fatfish/servlet/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.fatfish.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author fatfish
+ * @version 1.0
+ * @date 2024/6/8 22:39
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print("Hello, Servlet!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
