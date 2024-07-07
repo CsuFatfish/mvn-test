02c3e122f78c5c7f3776fedd972c4f39768869ee
Jiale <fatfishccc@gmail.com>
2024-07-07 19:52:27 +0800

    dev

--- Servlet-01/src/main/java/com/fatfish/servlet/HelloServlet.java
+++ Servlet-01/src/main/java/com/fatfish/servlet/HelloServlet.java
@@ -17,7 +17,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter printWriter = resp.getWriter();
-        printWriter.print("DEV-Hello, Servlet!");
+        printWriter.print("DEV-Hello, Servlet!-DEV");
     }
 
     @Override
